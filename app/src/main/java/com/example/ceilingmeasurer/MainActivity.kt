ee581bf7cd2d46c220b1c216bec791174cd849f2
Tambosa <tambosa@gmail.com>
2022-08-31 13:56:57 +0300

    Почистил мэйн

--- app/src/main/java/com/example/ceilingmeasurer/MainActivity.kt
+++ app/src/main/java/com/example/ceilingmeasurer/MainActivity.kt
@@ -1,24 +1,16 @@
 package com.example.ceilingmeasurer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.ceilingmeasurer.databinding.ActivityMainBinding
 
-private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        binding.button.setOnClickListener {
-            val fragment = PlanFragment.newInstance(binding.widthEt.text.toString(), binding.heightEt.text.toString())
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, fragment)
-//            transaction.addToBackStack(null)
-            transaction.commit()
-        }
+        setContentView(binding.root)
     }
 }
