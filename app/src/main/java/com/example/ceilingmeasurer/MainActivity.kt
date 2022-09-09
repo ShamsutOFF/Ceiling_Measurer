dcb3c081323f9a388e3e076f4ea73d230b8094ef
Tambosa <tambosa@gmail.com>
2022-09-09 20:25:56 +0300

    code cleanup

--- app/src/main/java/com/example/ceilingmeasurer/MainActivity.kt
+++ app/src/main/java/com/example/ceilingmeasurer/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.ceilingmeasurer
 
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ceilingmeasurer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
