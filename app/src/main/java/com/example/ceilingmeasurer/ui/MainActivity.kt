6539cd1cf56c8073c7a1aaf31276fbd36ad410a7
Tambosa <tambosa@gmail.com>
2022-09-14 20:20:22 +0300

    Fab

--- app/src/main/java/com/example/ceilingmeasurer/ui/MainActivity.kt
+++ app/src/main/java/com/example/ceilingmeasurer/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import com.example.ceilingmeasurer.R
 import com.example.ceilingmeasurer.databinding.ActivityMainBinding
 import com.example.ceilingmeasurer.ui.clientsList.ClientsListFragment
+import com.example.ceilingmeasurer.utils.IOnBackPressed
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -53,6 +54,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        //nothing
+        val fragment =
+            this.supportFragmentManager.findFragmentById(R.id.main_container)
+        (fragment as? IOnBackPressed)?.onBackPressed()?.not()?.let {
+            //nothing
+        }
     }
 }
--- app/src/main/java/com/example/ceilingmeasurer/ui/clientsList/ClientsListFragment.kt
+++ app/src/main/java/com/example/ceilingmeasurer/ui/clientsList/ClientsListFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.ceilingmeasurer.R
 import com.example.ceilingmeasurer.data.fake.FakeClientListRepoImpl
 import com.example.ceilingmeasurer.databinding.FragmentClientsListBinding
+import com.example.ceilingmeasurer.ui.ClientDetailsFragment
+import com.example.ceilingmeasurer.utils.IOnBackPressed
 
-class ClientsListFragment : Fragment() {
+class ClientsListFragment : Fragment(), IOnBackPressed {
 
     private var _binding: FragmentClientsListBinding? = null
     private val binding get() = _binding!!
@@ -28,16 +31,47 @@ class ClientsListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initRecycler()
+        iniFab()
+        initViewModel()
+        renderData()
+    }
+
+    private fun initRecycler() {
         binding.clientListRecyclerView.layoutManager = GridLayoutManager(context, 1)
         binding.clientListRecyclerView.adapter = adapter
+    }
+
+    private fun iniFab() {
+        binding.clientListFab.setOnClickListener {
+            initFragment(ClientDetailsFragment())
+        }
+    }
+
+    private fun initFragment(fragment: Fragment) {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.client_list_container, fragment)
+            .addToBackStack("")
+            .commit()
+    }
+
+    private fun initViewModel() {
         viewModel.clientList.observe(viewLifecycleOwner) {
             adapter.setData(it)
         }
+    }
+
+    private fun renderData() {
         viewModel.getClientList()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    override fun onBackPressed(): Boolean {
+        childFragmentManager.popBackStack()
+        return true
     }
 }
