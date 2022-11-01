a44f5e9b389e8a454fba787467865a8aa64b8a83
ShamsutOFF <shamsutoff@yandex.ru>
2022-11-01 14:09:34 +0300

    Перевел на URL Encode. Осталось доработать БД

--- mobile_counter/src/main/java/com/example/hellolibrary/MobileCounter.kt
+++ mobile_counter/src/main/java/com/example/hellolibrary/MobileCounter.kt
@@ -13,19 +13,23 @@ import org.json.JSONObject
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLEncoder
 import java.text.SimpleDateFormat
 import java.util.*
 
-
 private const val TAG = "@@@@@ Mobile Counter"
-private var androidID = ""
+private var deviceID = ""
 private var appName = ""
 private var launchCounter = 0
 private const val SAVED_DATA = "SavedData"
 private const val COUNTER = "Counter"
 
+private const val ENDPOINT_POSTMAN =
+    "https://42aea87e-bcb4-4f70-b387-7061bee87d95.mock.pstmn.io/orders/habr2"
 private const val ENDPOINT =
-    "https://42aea87e-bcb4-4f70-b387-7061bee87d95.mock.pstmn.io/orders/habr"
+    "https://stats.mos.ru/eds.gif?o="
+private const val ENDPOINT_TEST =
+    "https://stats.mos.ru/eds.gif?o=%7B%22eventType%22%3A%22mobile%22%2C%22eventSrc%22%3A%22Ceiling%20Measurer%22%2C%22eventDst%22%3A%22stats%22%2C%22eventTime%22%3A%201666867216%2C%22sso_id%22%3A%229180f787-65e3-11ed-bdc3-0242ac120002%22%2C%23eventObject%22%3A%20%7B%22launchCounter%22%3A%2025%2C%22deviceID%22%3A%22145feb91c6e32636%22%2C%22model%22%3A%22Android%20SDK%20built%20for%20x86%22%2C%22manufacturer%22%3A%22Google%22%2C%22board%22%3A%22goldfish_x86%22%2C%22brand%22%3A%22google%22%2C%22id%22%3A%22QSR1.210802.001%22%2C%22user%22%3A%22android-build%22%2C%22version.sdk_int%22%3A%2029%7D%7D"
 
 class MobileCounter {
     private lateinit var dao: MessageDao
@@ -35,11 +39,12 @@ class MobileCounter {
     fun init(context: Context) {
         val applicationInfo = context.applicationInfo
         val stringId = applicationInfo.labelRes
-        appName = if (stringId == 0) applicationInfo.nonLocalizedLabel.toString() else context.getString(
-            stringId
-        )
+        appName =
+            if (stringId == 0) applicationInfo.nonLocalizedLabel.toString() else context.getString(
+                stringId
+            )
         Log.d(TAG, "init() called with: appName = $appName")
-        androidID = Settings.Secure.getString(
+        deviceID = Settings.Secure.getString(
             context.contentResolver,
             Settings.Secure.ANDROID_ID
         )
@@ -61,40 +66,37 @@ class MobileCounter {
     private fun getDeviceInfoJSON(): JSONObject {
         val json = JSONObject()
         with(json) {
-            put("launchCounter", launchCounter)
-            put("androidID", androidID)
-            put("appName", appName)
-            put("MODEL", Build.MODEL)
-            put("MANUFACTURER", Build.MANUFACTURER)
-            put("BOARD", Build.BOARD)
-            put("BRAND", Build.BRAND)
-            put("ID", Build.ID)
-            put("USER", Build.USER)
-            put("VERSION.SDK_INT", Build.VERSION.SDK_INT)
+            put("launch_counter", launchCounter)
+            put("device_id", deviceID)
+            put("model", Build.MODEL)
+            put("manufacturer", Build.MANUFACTURER)
+            put("board", Build.BOARD)
+            put("brand", Build.BRAND)
+            put("id", Build.ID)
+            put("user", Build.USER)
+            put("version.sdk_int", Build.VERSION.SDK_INT)
         }
         return json
     }
 
     fun sendInfo(jsonObjectForSending: JSONObject) {
-        val formattedDate = formatter.format(Date())
-        jsonObjectForSending.put("Date", formattedDate)
         Thread {
-            val httpURLConnection = openHttpURLConnection()
+            val encodedDataString = getEncodedJsonAsString(jsonObjectForSending)
+            val url = URL(ENDPOINT + encodedDataString)
+            Log.d(TAG, "url = $url")
+            val httpURLConnection =
+                URL(ENDPOINT + encodedDataString).openConnection() as HttpURLConnection
+            httpURLConnection.requestMethod = "GET"
             try {
-                sendJsonToEndpoint(httpURLConnection, jsonObjectForSending)
                 if (httpURLConnection.responseCode == HttpURLConnection.HTTP_OK) {
-                    val json = httpURLConnection.inputStream.bufferedReader().readText()
-                    dao.deleteAll()
                     Log.d(TAG, "responseCode = ${httpURLConnection.responseCode}")
-                    Log.d(TAG, "json = $json")
+                    dao.deleteAll()
                 } else {
-                    val message = Message(0, jsonObjectForSending.toString())
-                    dao.insertMessage(message)
-                    Log.e("HTTPURLCONNECTION_ERROR", httpURLConnection.responseCode.toString())
+                    dao.insertMessage(Message(0, encodedDataString))
+                    Log.e("INTERCONNECTION_ERROR", httpURLConnection.responseCode.toString())
                 }
             } catch (ex: Exception) {
-                val message = Message(0, jsonObjectForSending.toString())
-                dao.insertMessage(message)
+                dao.insertMessage(Message(0, encodedDataString))
                 ex.printStackTrace()
             } finally {
                 httpURLConnection.disconnect()
@@ -102,34 +104,20 @@ class MobileCounter {
         }.start()
     }
 
-    private fun openHttpURLConnection(): HttpURLConnection {
-        Log.d(TAG, "openHttpURLConnection() called")
-        val httpURLConnection = URL(ENDPOINT).openConnection() as HttpURLConnection
-        with(httpURLConnection) {
-            requestMethod = "POST"
-            setRequestProperty("Content-Type", "application/json")
-            setRequestProperty("Accept", "application/json")
-            doInput = true
-            doOutput = true
-        }
-        return httpURLConnection
-    }
-
-    private fun sendJsonToEndpoint(
-        httpURLConnection: HttpURLConnection,
-        jsonObjectForSending: JSONObject
-    ) {
+    private fun getEncodedJsonAsString(jsonObjectForSending: JSONObject): String {
         val jsonForSending = JSONObject()
-        jsonForSending.accumulate("DeviceInfo", getDeviceInfoJSON())
+        jsonForSending.put("eventType", "mobile")
+        jsonForSending.put("eventSrc", appName)
+        jsonForSending.put("eventDst", "stats")
+        jsonForSending.put("eventTime", Date().time)
+        jsonForSending.put("sso_id", UUID.randomUUID().toString())
+        jsonForSending.put("eventObject", getDeviceInfoJSON())
+        jsonForSending.accumulate("eventObject", jsonObjectForSending)
+        Log.d(TAG, "getEncodedJsonAsString() called jsonForSending = $jsonForSending")
 
-        val messagesList = dao.getAll()
-        messagesList.forEach {
-            jsonForSending.accumulate("Action", it.message)
-        }
-        jsonForSending.accumulate("Action", jsonObjectForSending)
-        Log.d(TAG, "sendInfo() called jsonForSending = $jsonForSending")
-        val outputStreamWriter = OutputStreamWriter(httpURLConnection.outputStream)
-        outputStreamWriter.write(jsonForSending.toString())
-        outputStreamWriter.flush()
+        val encodedJSONSting = URLEncoder.encode(jsonForSending.toString(), "UTF-8")
+        Log.d(TAG, "getEncodedJsonAsString() called encodedJSONSting = $encodedJSONSting")
+
+        return encodedJSONSting
     }
 }
